e219981953424996b860d6eeebb0a8eeaa4dde06
Aleksandr Kartashov <aleksandrmon1@gmail.com>
2021-10-16 17:29:17 +0300

    rename url

--- src/main/java/com/example/alpha/clients/RateClient.java
+++ src/main/java/com/example/alpha/clients/RateClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "client_rate", url = "${service.rate.link}")
+@FeignClient(name = "client_rate", url = "${service.rate.url}")
 public interface RateClient {
 
     @GetMapping("/latest.json?app_id=${app_id}")
