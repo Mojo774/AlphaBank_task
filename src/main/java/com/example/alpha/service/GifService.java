923c7bb63eb64c46046c6a6b8790aec30bba8eb9
Aleksandr Kartashov <aleksandrmon1@gmail.com>
2021-10-16 11:33:54 +0300

    add method getGif

--- src/main/java/com/example/alpha/service/GifService.java
+++ src/main/java/com/example/alpha/service/GifService.java
@@ -1,5 +1,7 @@
 package com.example.alpha.service;
 
+import com.example.alpha.clients.GifClient;
+import com.example.alpha.models.TypeGif;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
@@ -8,12 +10,15 @@ import org.springframework.stereotype.Controller;
 public class GifService {
 
     @Autowired
-    private GifService gifService;
+    private GifClient gifClient;
 
     @Value("${api_key}")
     private String api_key;
 
-    public String getGif(int t){
-        return "r";
+    public String getGif(TypeGif typeGif){
+
+        String result = gifClient.findGif(api_key,typeGif.toString());
+
+        return result;
     }
 }
