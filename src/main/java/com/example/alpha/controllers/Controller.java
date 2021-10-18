0ff40f20ebb41705fe55dba8f9ed308ea9a06113
Aleksandr Kartashov <aleksandrmon1@gmail.com>
2021-10-18 16:42:23 +0300

    added error processing

--- src/main/java/com/example/alpha/controllers/Controller.java
+++ src/main/java/com/example/alpha/controllers/Controller.java
@@ -3,16 +3,16 @@ package com.example.alpha.controllers;
 import com.example.alpha.models.TypeGif;
 import com.example.alpha.service.GifService;
 import com.example.alpha.service.RateService;
+import com.example.alpha.util.RequestHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpServletResponseWrapper;
 import java.net.URI;
 
 @RestController
@@ -28,13 +28,18 @@ public class Controller {
     @GetMapping()
     public ResponseEntity getGif() {
 
-        TypeGif t = rateService.getTypeGif();
-        String response = gifService.getGif(t);
+        TypeGif typeGif = rateService.getTypeGif();
+        String response = gifService.getGifLink(typeGif);
 
         HttpHeaders headers = new HttpHeaders();
         headers.setLocation(URI.create(response));
 
         return new ResponseEntity(headers, HttpStatus.SEE_OTHER);
+    }
+
+    @ExceptionHandler(Exception.class)
+    private ResponseEntity handleException(Exception e) {
+        return RequestHandler.getBadRequest(e);
     }
 
 }
